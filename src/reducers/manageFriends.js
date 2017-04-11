013111c93f0e2414bfdbd42395bb823c1b9058c6
fakebenjay <fakebenjay@gmail.com>
2017-04-11 16:19:37 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  if (action.type === "ADD_FRIEND") {
+    return Object.assign({}, state, {
+      friends: [...state.friends, action.friend]
+    })
+  }
+  else if (action.type === "REMOVE_FRIEND") {
+    const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+    return Object.assign({}, state, {
+      friends: [
+        ...state.friends.slice(0, removalIndex),
+        ...state.friends.slice(removalIndex + 1)
+      ]
+    })
+  }
+  return state
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  if (action.type === "INCREASE") {
+    return Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents + 1
+    })
+  }
+  else if (action.type === "DECREASE") {
+    return Object.assign({}, state, {
+      numberOfPresents: state.numberOfPresents - 1
+    })
+  }
+  return state
 }
