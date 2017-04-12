08d7c83c23b29e2a2b83b8efbf6757607903a81e
chasms <chasms@chas.ms>
2017-04-12 12:14:08 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND') {
+    let newState =  { ...state, friends: state.friends.concat(action.friend) }
+    return newState
+  } else if (action.type === 'REMOVE_FRIEND') {
+    let newState =  { ...state, friends: state.friends.filter((obj) => {
+      return obj.id !== action.id
+    })}
+    return newState
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  if (action.type === 'INCREASE') {
+    let newState =  { ...state, numberOfPresents: state.numberOfPresents + 1 }
+    return newState
+  } else {
+    return state
+  }
 }
